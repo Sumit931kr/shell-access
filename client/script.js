8c6bbbf66a13ed830804ec65db44fa26296b0b91
Sumit <sumitkumar280601@gmail.com>
2024-09-04 22:37:12 +0530

    add security

---
+++ client/script.js
@@ -0,0 +1,237 @@
+const editableDiv = document.getElementById('editableDiv');
+const passcode_container = document.getElementById('passcode_container');
+const passcode_input = document.getElementById('input_passcode');
+const passcode_msg = document.querySelector('.passcode-msg');
+
+function initialization_UI () {
+let authToken = sessionStorage.getItem('authToken');
+
+editableDiv.style.display = `${authToken ? "block":"none"}`;
+passcode_container.style.display = `${authToken ? "none":"flex"}`
+
+if(authToken){
+    editableDiv.focus();
+    setTimeout(() => {
+        handleMoveCursorToEnd();
+    }, 500);
+}
+else{
+    passcode_input.focus()
+}
+
+}
+initialization_UI();
+
+
+const socket = io('/', {
+    path: '/shell-access/socket.io/other'
+    // path: '/socket.io/other'
+});
+
+const verifyPassCode = ()=> {
+let input = document.getElementById('input_passcode');
+// console.log(input.value);
+socket.emit("passcode-verify",input.value);
+
+}
+
+
+// 
+let previousCommandsArray = [];
+let commandCounter = previousCommandsArray.length;
+
+
+
+
+const editableDivClick = (event) =>  {
+
+    if (event.key === 'Enter') {
+        event.preventDefault();
+
+        currentLine = editableDiv.innerHTML
+        currentLine = currentLine.trim()
+        // console.log("currentLineArr ")
+
+        // cutting some lines
+        currentLineArr = currentLine.split("</span>");
+        // console.log(currentLineArr)
+        currentLine = currentLineArr[currentLineArr.length - 1]
+
+        // console.log(currentLine)
+        if (currentLine[currentLine.length - 1] == '>') {
+            currentLine = "";
+        }
+        // console.log(currentLine)
+        sendCommandHandler(currentLine)
+        handleMoveCursorToEnd();
+    }
+
+    if (event.key == 'ArrowUp') {
+        // console.log("arraow up");
+        if (commandCounter > 0) {
+            commandCounter--;
+        }
+
+        if (previousCommandsArray[commandCounter]) {
+            let allCommandArr = editableDiv.innerHTML.split('</span>');
+
+            allCommandArr.pop();
+
+            let allCommandStr = allCommandArr.join('</span>') + "</span>";
+
+            let allCommand = `${allCommandStr}${previousCommandsArray[commandCounter]}`;
+
+            editableDiv.innerHTML = allCommand;
+
+        }
+        handleMoveCursorToEnd();
+    }
+    if (event.key == 'ArrowDown') {
+        // console.log("arrow down");
+        if (commandCounter <= previousCommandsArray.length - 1) {
+            commandCounter++;
+        }
+        // console.log("Command counter "+ commandCounter)      
+        // console.log(previousCommandsArray[commandCounter])
+        // console.log(previousCommandsArray[commandCounter])
+        if (previousCommandsArray[commandCounter]) {
+            let allCommandArr = editableDiv.innerHTML.split('</span>');
+
+            allCommandArr.pop();
+
+            let allCommandStr = allCommandArr.join('</span>') + "</span>";
+
+            let allCommand = `${allCommandStr}${previousCommandsArray[commandCounter]}`;
+
+            editableDiv.innerHTML = allCommand;
+        }
+        else {
+            let allCommandArr = editableDiv.innerHTML.split('</span>');
+
+            allCommandArr.pop();
+
+            let allCommandStr = allCommandArr.join('</span>') + "</span>";
+            editableDiv.innerHTML = allCommandStr
+        }
+        handleMoveCursorToEnd();
+    }
+
+
+    setTimeout(() => {
+        editableDiv.scrollTop = editableDiv.scrollHeight
+    }, 100);
+};
+
+
+
+
+socket.on('connect', () => {
+    socket.emit('shell-connection', "ha");
+
+    socket.on("passcode-verified", authToken=>{
+        // console.log("AuthToken ", authToken);
+        sessionStorage.setItem('authToken', authToken);
+        initialization_UI();
+    })
+    
+    socket.on("passcode-failed",(wrong)=>{
+        passcode_msg.style.display = "block";
+        sessionStorage.clear();
+        initialization_UI();
+    })
+    
+    socket.on("start", (dir) => {
+        dir += " >"
+        editableDiv.innerHTML = `<span class="dir" contenteditable="false">${dir}</span> `
+        moveCursorToEnd(editableDiv)
+    })
+    socket.on("result", (res) => {
+        // console.log(res)           
+        editableDiv.innerHTML += `<pre>${res}</pre>`
+    
+    })
+    
+    socket.on("givedir", (dir) => {
+    
+        dir += " >"
+        editableDiv.innerHTML += '<br/>' + `<span class="dir" contenteditable="false">${dir}</span> `
+    
+        editableDiv.scrollTop = editableDiv.scrollHeight
+        moveCursorToEnd(editableDiv)
+        // setCursorPosition(editableDiv, 135);
+    })
+});
+
+
+
+// send command handler
+const sendCommandHandler = (command) => {
+    if (!command) return
+    // console.log(command)
+
+    command = command.replaceAll('&nbsp;', ' ');
+
+    command = command.trim()
+    let sendAbleCommand = validateSentence(command);
+    // console.log("Send command " + sendAbleCommand);
+
+    previousCommandsArray.push(sendAbleCommand)
+    if (sendAbleCommand == 'clear') { window.location.reload(); return; }
+
+    let result = {
+        authToken : sessionStorage.getItem('authToken'),
+        command : sendAbleCommand
+    }
+    socket.emit('sendcommand', result);
+    commandCounter = previousCommandsArray.length;
+
+
+
+}
+
+
+// move the cursor to the end
+function moveCursorToEnd(element) {
+    // Make sure the element is contentEditable
+    element.contentEditable = true;
+
+    // Create a new range
+    let range = document.createRange();
+
+    // Select the element
+    range.selectNodeContents(element);
+
+    // Collapse the range to the end
+    range.collapse(false);
+
+    // Get the selection object
+    let selection = window.getSelection();
+
+    // Remove any existing selections
+    selection.removeAllRanges();
+
+    // Add the new range
+    selection.addRange(range);
+
+    // Focus on the element
+    element.focus();
+}
+
+const handleMoveCursorToEnd = () => {
+    setTimeout(() => { moveCursorToEnd(editableDiv) }, 300)
+
+}
+
+
+// get previous commands
+
+
+
+// validate Sentence 
+
+const validateSentence = (sentence) => {
+    if (!sentence) return "";
+
+    return sentence.split(' ').map((el) => el.trim()).filter((el) => el !== "").join(' ')
+
+}
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-function initialization() {
+
+
+
+function initialization(passCode) {
 
     console.log("started");
     const { exec } = require('child_process');;
@@ -11,26 +14,61 @@ function initialization() {
 
     const port = 8765;
 
+    // const passCode = passCode;
+
 
     const server = http.createServer((req, res) => {
-        // Serve the .html file
-        const filePath = path.join(__dirname, 'index.html'); // Replace 'index.html' with your file name
+        // Get the file path from the URL
+        let filePath = path.join(__dirname, 'client', req.url === '/' ? 'index.html' : req.url);
 
-        fs.readFile(filePath, (err, data) => {
+        // Get the file extension
+        const extname = path.extname(filePath);
+
+        // Set the content type based on the file extension
+        let contentType = 'text/html';
+        switch (extname) {
+            case '.js':
+                contentType = 'text/javascript';
+                break;
+            case '.css':
+                contentType = 'text/css';
+                break;
+            case '.json':
+                contentType = 'application/json';
+                break;
+            case '.png':
+                contentType = 'image/png';
+                break;
+            case '.jpg':
+                contentType = 'image/jpg';
+                break;
+        }
+
+        // Read the file
+        fs.readFile(filePath, (err, content) => {
             if (err) {
-                res.statusCode = 500;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('Internal Server Error');
+                if (err.code === 'ENOENT') {
+                    // File not found
+                    fs.readFile(path.join(__dirname, 'client', '404.html'), (err, content) => {
+                        res.writeHead(404, { 'Content-Type': 'text/html' });
+                        res.end(content, 'utf-8');
+                    });
+                } else {
+                    // Server error
+                    res.writeHead(500);
+                    res.end(`Server Error: ${err.code}`);
+                }
             } else {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/html');
-                res.end(data);
+                // Success
+                res.writeHead(200, { 'Content-Type': contentType });
+                res.end(content, 'utf-8');
             }
         });
     });
 
+
     const io = require("socket.io")(server.listen(port, () => {
-        console.log(`Server running at http://localhost:${port}/`);
+        // console.log(`Server running at http://localhost:${port}/`);
     }), {
         path: '/socket.io/other',
         cors: {
@@ -43,26 +81,42 @@ function initialization() {
 
     io.on('connection', socket => {
 
-        socket.on("sendcommand", command => {
-            console.log("command " + command);
+        socket.on("passcode-verify", passcode => {
+            // console.log(passcode);
+            if (passcode == passCode) {
+                socket.emit('passcode-verified', encrypt(passCode));
+            }
+            else {
+                socket.emit('passcode-failed', "wrong")
+            }
+        })
+
+        socket.on("sendcommand", ({authToken,command}) => {
+
+            let decryptText = decrypt(authToken);
+            console.log(decryptText)
+            if(decryptText !== passCode) {
+                socket.emit('passcode-failed', "wrong")
+            }
 
             let isError = false;
 
-            if(command == 'cd /'){
+            if (command == 'cd /') {
                 console.log("cd / ----  found");
                 directoryControl = ""
             }
 
             // execute commands
-            let executeCommand = directoryControl ? `cd  ${directoryControl} && ${command}` : command
-            executeCommand = executeCommand.replaceAll('&nbsp;', ' ');
-            exec(executeCommand, (err, stdout, stderr) => {
+            let execuetCommand = directoryControl ? `cd  ${directoryControl} && ${command}` : command
+            console.log("execu command " + execuetCommand);
+            execuetCommand = execuetCommand.replaceAll('&nbsp;', ' ');
+            exec(execuetCommand, (err, stdout, stderr) => {
                 if (err) {
                     console.log("erro " + err)
                     console.log(err)
-                    socket.emit('result', stderr);
+                    // socket.emit('result', stderr);
                     isError = true;
-                    
+
                 }
                 else if (stderr) {
                     console.log(`stderr: ${stderr}`);
@@ -71,16 +125,16 @@ function initialization() {
 
                 }
 
-                console.log("dir  " + directoryControl)
+                // console.log("dir  " + directoryControl)
 
-                if ( !isError && command.search('cd ') == 0) {
+                if (!isError && command.search('cd ') == 0) {
                     console.log("cd found");
 
 
                     let dir = command.split(' ');
                     let dotdir = dir[1];
                     console.log(dotdir);
-                    let addCmd = dotdir + (dotdir[dotdir.length-1] == '/' ? "" : "/")
+                    let addCmd = dotdir + (dotdir[dotdir.length - 1] == '/' ? "" : "/")
                     console.log(addCmd)
                     directoryControl += addCmd;
                 }
@@ -115,7 +169,7 @@ function initialization() {
                 console.log(`stderr: ${stderr}`);
             }
             // the *entire* stdout and stderr (buffered)
-            console.log(`stdout: ${stdout}`);
+            // console.log(`stdout: ${stdout}`);
             socket.emit('start', stdout)
         });
 
@@ -132,7 +186,36 @@ function initialization() {
 
 }
 
-// initialization();
+// initialization("sumit");
+
+
+function encrypt(text, shift) {
+    if (!shift) shift = 3;
+    return text.split('').map(char => {
+        if (char.match(/[a-z]/i)) {
+            const code = char.charCodeAt(0);
+            const isUpperCase = char === char.toUpperCase();
+            const base = isUpperCase ? 65 : 97;
+            return String.fromCharCode(((code - base + shift) % 26) + base);
+        }
+        return char;
+    }).join('');
+}
+
+function decrypt(text, shift) {
+    if (!shift) shift = 3;
+    return encrypt(text, 26 - shift);
+}
+
+// Example usage:
+//   const originalText = "Hello, World!";
+//   const shiftAmount = 3;
+
+//   const encryptedText = encrypt(originalText, shiftAmount);
+//   console.log("Encrypted:", encryptedText);
+
+//   const decryptedText = decrypt(encryptedText, shiftAmount);
+//   console.log("Decrypted:", decryptedText);
 
 module.exports = initialization;
 
